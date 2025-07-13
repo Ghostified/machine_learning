73dc99a800e5c76834fb5a3dbafa16191fd8f839
Ghostified <bransonallan@gmail.com>
2025-07-14 02:40:43 +0300

    happy birthday branson

---
+++ step4_univariate_plots.py
@@ -0,0 +1,34 @@
+# %%
+#Data Visualization
+#Univariate plots -- plots for each variable
+#load  liblaries 
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm  import SVC
+from sklearn.naive_bayes import GaussianNB
+
+
+# %%
+#load  datasets
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length','petal-width', 'class']
+dataset=read_csv(url, names=names)
+
+# %%
+# Univariate plot, i.e plots for each variablle 
+#given that each input variable are numeric , box and whisker for each
+dataset.plot(kind='box',subplots=True, layout=(2,2), sharex=False)
+plt.show()
+
+# %%
--- two_import_liblaries.py
+++
@@ -1,6 +0,0 @@
-#load  datasets 
-from pandas import read.csv
-from pandas.plotting import scatter_matrix
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-
