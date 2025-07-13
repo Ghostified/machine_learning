426ce855f0a2cf2067b9dd2427934925f224ba47
Ghostified <bransonallan@gmail.com>
2025-07-13 23:54:16 +0300

    dev - Hello 30'

---
+++ two_import_liblaries.py
@@ -0,0 +1,6 @@
+#load  datasets 
+from pandas import read.csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
---
+++ versions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#check for versions and liblaries
+# python version
+import sys
+import sklearn
+import scipy
+import numpy
+import matplotlib
+import pandas
+print('Python: {}'.format(sys.version))
+print('scipy: {}'.format(scipy.__version__))
+print('numpy: {}'.format(numpy.__version__))
+print('matplotlib: {}'.format(matplotlib.__version__))
+print('pandas: {}'.format(pandas.__version__))
+print('sklearn: {}'.format(sklearn.__version__))
+
+
+
